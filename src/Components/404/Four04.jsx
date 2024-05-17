211f3cb56c8fbe2267a461409e78496f6982e8f0
eualx <eualx14j@gmail.com>
2024-05-17 12:12:25 +0300

    loginpage

---
+++ src/Components/404/Four04.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './four04.css'
+
+const Four04 = () => {
+  return (
+    <div className='for04'>
+<h1>Lost your way?</h1>
+<p>Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+    </div>
+  )
+}
+
+export default Four04
